c3d2de5d694c4a5768ee09976bf6ab31514b59f3
Oluwasegun Adeleye <oluwasegun@dealdey.com>
2015-06-10 11:47:39 +0100

    Add scopes to the User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,17 +12,43 @@ class User < ActiveRecord::Base
   has_many :friend_requests
   has_many :senders, through: :friend_requests
 
-  scope :active, ->{ where.not(active: false, invitation_accepted_at: nil) }
-  scope :registered, ->{ where.not(invitation_accepted_at: nil).order("active DESC") }
+  has_many :accepted_friend_requests, -> { where accepted: true }, class_name: "FriendRequest"
+  has_many :unaccepted_friend_requests, -> { where accepted: false }, class_name: "FriendRequest"
+  
+  has_many :pending_friend_requests, -> { where accepted: false }, class_name: "FriendRequest", foreign_key: "sender_id"
+
+  default_scope { where.not(invitation_accepted_at: nil) }
+  # default_scope { where("created_at IS NULL") }
+  # default_scope { where("active = 1") }
+
+  scope :active, ->{ where(active: true).order(created_at: :DESC) }
+
+  # scope :active, ->{ where.not(active: false, invitation_accepted_at: nil).order(created_at: :DESC) }
+  # scope :registered, ->{ where.not(invitation_accepted_at: nil).order("active DESC") }
   scope :yet_to_accept_invitation, ->{ where(invitation_accepted_at: nil) }
   scope :inactive, ->{ where(active: false)}
+
+  # users with unaccepted friend requests
+  scope :with_unaccepted_friend_request_recieved, -> {
+    joins(:friend_requests).merge(FriendRequest.unaccepted)
+  }
+
+  scope :with_unaccepted_friend_request_sent, -> {
+    joins(:friend_requests).merge(FriendRequest.unaccepted)
+  }
+
+  # even though #pluck is a shortcut for #map, this will not work when #pluck is called
+  # def name
+  #   "I am #{super}"
+  # end
 
   def my_friends_ids
     @friends_ids ||= friends.pluck(:id) + inverse_friends.pluck(:id)
   end
 
   def my_friends
-    @friends ||= User.where(id: my_friends_ids)
+    @friends ||= User.where(id: my_friends_ids) #=> returns an ActiveRecord_Relation
+    # @friends ||= User.find(my_friends_ids) #=> returns an Array
   end
 
   def active_for_authentication?
